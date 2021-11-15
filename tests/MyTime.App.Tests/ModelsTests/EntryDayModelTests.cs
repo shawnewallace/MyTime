c7edf71259e28b1889a981f1d8ef3c866e7238d0
Shawn Wallace <shawn@the-wallaces.net>
2021-11-15 16:06:17 -0500

    refactoring

--- tests/MyTime.App.Tests/ModelsTests/EntryDayModelTests.cs
+++ tests/MyTime.App.Tests/ModelsTests/EntryDayModelTests.cs
@@ -10,12 +10,12 @@ namespace MyTime.App.Models.Tests
 
 		public EntryDayModelTests() => _day = new EntryDay(2021, 5, 14);
 
-		[Fact] public void YearShouldBe202() => _day.Year.ShouldBe(2021);
-		[Fact] public void MonthShouldBe5() => _day.Month.ShouldBe(5);
 		[Fact] public void DayOfMonthShouldBe14() => _day.DayOfMonth.ShouldBe(14);
-		[Fact] public void EntriesIsInstantiatedByDefault() => _day.Entries.ShouldNotBeNull();
 		[Fact] public void EntriesIsEmptyByDefault() => _day.Entries.ShouldBeEmpty();
+		[Fact] public void EntriesIsInstantiatedByDefault() => _day.Entries.ShouldNotBeNull();
+		[Fact] public void MonthShouldBe5() => _day.Month.ShouldBe(5);
 		[Fact] public void TotalIsZeroWithoutEntries() => _day.Total().ShouldBe(0.0F);
+		[Fact] public void YearShouldBe202() => _day.Year.ShouldBe(2021);
 
 		[Fact]
 		public void TotalSumsAllDurations()
