fa7586c0121b1f2fe78fb47ea397958c48892354
Shawn Wallace <shawn@the-wallaces.net>
2022-01-06 15:19:24 -0500

    still working on web app framework

--- src/my-time-app/src/index.js
+++ src/my-time-app/src/index.js
@@ -1,11 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 
 ReactDOM.render(
   <React.StrictMode>
--- src/mytime-web/src/App.js
+++ src/mytime-web/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Nav from './components/Nav/Nav'
 import Month from './components/Month/Month'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 function App() {
   return (
