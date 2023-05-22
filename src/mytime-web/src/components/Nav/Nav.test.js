c07b03cbf2f75d52af8236d8dee9dc13bfb5a09e
Shawn Wallace <shawnewallace@users.noreply.github.com>
2023-05-22 13:55:38 -0400

    removing old projects

--- src/mytime-web/src/components/Nav/Nav.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Nav from './Nav';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Nav />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
