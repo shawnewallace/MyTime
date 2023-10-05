a00a2909d8689b3ace34ae49d250ef67bb754dfc
Shawn Wallace <shawnewallace@users.noreply.github.com>
2023-10-05 16:00:50 -0400

    added very basic report

--- src/timesheet-app/src/components/Reporting/RangeView.js
+++ src/timesheet-app/src/components/Reporting/RangeView.js
@@ -6,6 +6,8 @@ const RangeView = () => {
 	let today = new Date();
 	const [entries, setEntries] = useState([]);
 	const [selectedFirstDayOfMonth, setSelectedFirstDayOfMonth] = useState(new Date(today.getFullYear(), today.getMonth(), 1));
+	const [billable, setBillable] = useState(0);
+	const [total, setTotal] = useState(0);
 
 	const firstDayOfTheMonth = moment(selectedFirstDayOfMonth).format('YYYY-MM-DD');
 	const lastDayOfTheMonth = moment(selectedFirstDayOfMonth).endOf('month').format('YYYY-MM-DD');
@@ -21,8 +23,8 @@ const RangeView = () => {
 
 		const data = await apiService.getEntries(begin, end);
 		setEntries(data.entries);
-		// setBillable(data.utilizedTotal);
-		// setTotal(data.total);
+		setBillable(data.utilizedTotal);
+		setTotal(data.total);
 	}
 
 	const handleDateChange = (date) => {
@@ -31,9 +33,6 @@ const RangeView = () => {
 	};
 
 	const handleDateIncrement = (increment) => {
-		let newDate = moment(selectedFirstDayOfMonth).add(increment, 'months').toDate();
-
-alert(newDate);
 		handleDateChange(moment(selectedFirstDayOfMonth).add(increment, 'months').toDate());
 	};
 
@@ -45,8 +44,8 @@ alert(newDate);
 			<div className="row">
 				<div className="col">
 					<div className="flex-bind">
-						
-						Entries for month starting: 
+
+						Entries for month starting:
 						<button type='button' className='btn btn-light btn-sm' onClick={() => handleDateIncrement(-1)}>
 							<i className='bi bi-arrow-left-square'></i>
 						</button>
@@ -57,11 +56,33 @@ alert(newDate);
 						and ending: {lastDayOfTheMonth}
 					</div>
 				</div>
-				<div className="row">
-					{entries.length > 0 ? (<></>) : (
-						<p>No entries for this month.</p>
-					)}
-				</div>
+
+				{entries.length > 0 ? (
+					<>
+						<div className="row">
+							<div className="col">&nbsp;</div>
+							<div className="col">&nbsp;</div>
+							<div className="col float-right">
+								Total: {total.toFixed(2)} hours <br />
+								Billable: {billable.toFixed(2)} hours
+							</div>
+							<div className="col">&nbsp;</div>
+						</div>
+
+						{entries.map((entry, index) => (
+							<div className="row" key={entry.id}>
+								<div className="col">
+									{moment(entry.onDate).format("YYYY-MM-DD")}
+								</div>
+								<div className="col">{entry.description}</div>
+								<div className="col">{entry.category}</div>
+								<div className="col float-right">{entry.duration.toFixed(2)}</div>
+								<div className="col">{entry.isUtilization ? "YES" : "NO"}</div>
+							</div>
+						))}
+					</>) : (
+					<div className="row"><p>No entries for this month.</p></div>
+				)}
 			</div>
 		</div>
 	);
