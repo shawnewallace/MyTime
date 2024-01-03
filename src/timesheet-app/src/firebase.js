8deeedf0164db7945f543b870120cdc30f420486
Shawn Wallace <shawnewallace@users.noreply.github.com>
2024-01-03 12:46:06 -0500

    adding config for auth to the app

--- src/timesheet-app/src/firebase.js
+++ src/timesheet-app/src/firebase.js
@@ -6,13 +6,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyBCj344y01yooWoWOIxRNseOFm1JS3Rd9g",
-	authDomain: "mytime-24300.firebaseapp.com",
-	projectId: "mytime-24300",
-	storageBucket: "mytime-24300.appspot.com",
-	messagingSenderId: "416410848775",
-	appId: "1:416410848775:web:8e05d82fbfcbf2ef9d24a2"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
